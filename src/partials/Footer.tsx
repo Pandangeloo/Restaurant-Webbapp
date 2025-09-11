70157deb1d8f7c98e226a9b861a5e041ade75aa3
Angelica <angelicanorlander@telia.com>
2025-09-11 21:19:41 +0200

    Design, Namechanges and Colorchanges

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,13 +1,15 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Footer() {
-  return <footer>
-    <Container fluid>
-      <Row>
-        <Col className="text-center py-3 text-bg-primary">
-          © The Good Grocery {new Date().getFullYear()}
-        </Col>
-      </Row>
-    </Container>
-  </footer>;
+  return (
+    <footer>
+      <Container fluid>
+        <Row>
+          <Col className="text-center py-3 text-bg-primary">
+            © The Axolotl {new Date().getFullYear()}
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
