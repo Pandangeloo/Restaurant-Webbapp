86fa11ef9a25f4d306d45cb2efcc8bae97f6506a
Angelica <angelicanorlander@telia.com>
2025-09-15 14:14:30 +0200

    Feature-Footer :  Install reacticons fpr socialmediaIcons

---
+++ src/parts/FooterParts/SocialIcons.tsx
@@ -0,0 +1,16 @@
+//Todo: I want 4 icons from socialmedia. when you click onn them u go to another website.
+import { BsInstagram } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { BsTiktok } from "react-icons/bs";
+
+export default function SocialIcons() {
+  return (
+    <>
+      <BsInstagram />
+      <BsFacebook />
+      <BsLinkedin />
+      <BsTiktok />
+    </>
+  );
+}
