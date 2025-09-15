038142c9e056a339ccebeaec9e758755b779e9a8
Angelica <angelicanorlander@telia.com>
2025-09-15 09:36:40 +0200

    The original example

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,15 +1,13 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Footer() {
-  return (
-    <footer>
-      <Container fluid>
-        <Row>
-          <Col className="text-center py-3 text-bg-primary">
-            © The Axolotl {new Date().getFullYear()}
-          </Col>
-        </Row>
-      </Container>
-    </footer>
-  );
+  return <footer>
+    <Container fluid>
+      <Row>
+        <Col className="text-center py-3 text-bg-primary">
+          © The Good Grocery {new Date().getFullYear()}
+        </Col>
+      </Row>
+    </Container>
+  </footer>;
 }
