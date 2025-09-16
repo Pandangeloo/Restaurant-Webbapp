26dfbda7abf89bbd2b236610f1e988d16cc5ca92
Angelica <angelicanorlander@telia.com>
2025-09-16 11:47:21 +0200

    Feature-Footer :  First try on props and adjusted links to more amusing links

--- src/parts/FooterParts/SocialIcons.tsx
+++ src/parts/FooterParts/SocialIcons.tsx
@@ -1,20 +1,51 @@
-import { BsInstagram, BsFacebook, BsLinkedin, BsTiktok } from "react-icons/bs";
+import { BsInstagram, BsFacebook, BsTiktok, BsYoutube } from "react-icons/bs";
 
-export default function SocialIcons() {
+type Props = {
+  className?: string;
+  target?: string;
+  rel?: string;
+};
+
+//_blank is used so that a new webpage opens. and noopende norefferer is used so the new page cant access my page for malicious purpose
+export default function SocialIcons({
+  className = "link-light",
+  target = "_blank",
+  rel = "noopener noreferrer",
+}: Props) {
   return (
-    <>
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+    <div className="d-flex gap-3 fs-3">
+      <a
+        className={className}
+        href="https://www.instagram.com/reel/DIzZpOXpcWo/?hl=en"
+        target={target}
+        rel={rel}
+      >
         <BsInstagram />
       </a>
-      <a href="https://facebook.com" target="_blank" rel="noopener norefferer">
+      <a
+        className={className}
+        href="https://www.facebook.com/groups/axolotlcare/"
+        target={target}
+        rel={rel}
+      >
         <BsFacebook />
       </a>
-      <a href="https://linkedin.com" target="_blank" rel="noopener norefferer">
-        <BsLinkedin />
-      </a>
-      <a href="https://tiktok.com" target="_blank" rel="noopener norefferer">
+      <a
+        className={className}
+        href="https://www.tiktok.com/@aquariumpacific/video/7464035293751184686"
+        target={target}
+        rel={rel}
+      >
         <BsTiktok />
       </a>
-    </>
+      <a
+        className={className}
+        href="https://www.youtube.com/shorts/OvD30K-KN3k?feature=share"
+        target={target}
+        rel={rel}
+      >
+        <BsYoutube />
+      </a>
+    </div>
   );
 }
