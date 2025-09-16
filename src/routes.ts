4f992cdb9adcf8fa222fadf33462cb74a65522d0
Angelica <angelicanorlander@telia.com>
2025-09-16 13:19:06 +0200

    Cleanup : Removed stuff from the original example that i dont use

--- src/routes.ts
+++ src/routes.ts
@@ -2,27 +2,13 @@ import type Route from "./interfaces/Route.ts";
 import { createElement } from "react";
 
 // page components
-import AboutPage from "./pages/AboutPage.tsx";
+
 import NotFoundPage from "./pages/NotFoundPage.tsx";
-import OurVisionPage from "./pages/OurVisionPage.tsx";
-import ProductDetailsPage from "./pages/ProductDetailsPage.tsx";
-import ProductsPage from "./pages/ProductsPage.tsx";
 import BookTablePage from "./pages/BookTablePage.tsx";
 import EventsPage from "./pages/EventsPage.tsx";
 import MenuPage from "./pages/MenuPage.tsx";
 
-
-export default [
-  AboutPage,
-  NotFoundPage,
-  OurVisionPage,
-  ProductDetailsPage,
-  ProductsPage,
-  BookTablePage,
-  EventsPage,
-  MenuPage,
-
-]
+export default [NotFoundPage, BookTablePage, EventsPage, MenuPage]
   // map the route property of each page component to a Route
   .map((x) => ({ element: createElement(x), ...x.route } as Route))
   // sort by index (and if an item has no index, sort as index 0)
