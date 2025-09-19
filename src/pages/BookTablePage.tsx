f78c26d4d65cb9642b6008bdbdf5a74a74d7634e
Angelica <angelicanorlander@telia.com>
2025-09-19 14:46:42 +0200

    Feature-BookATable : Added nonworking forms

--- src/pages/BookTablePage.tsx
+++ src/pages/BookTablePage.tsx
@@ -1,5 +1,6 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form, Button } from "react-bootstrap";
 import Image from "../parts/Image";
+import { useState } from "react";
 
 BookTablePage.route = {
   path: "/book-a-table",
@@ -8,6 +9,12 @@ BookTablePage.route = {
 };
 
 export default function BookTablePage() {
+  const [form] = useState({
+    name: "",
+    guests: "",
+    reservation_time: "",
+    email: "",
+  });
   return (
     <>
       <Row>
@@ -22,6 +29,35 @@ export default function BookTablePage() {
             src="/images/BookTable.jpg"
             alt="A group photo of our employees."
           />
+
+          <Form.Group>
+            <Form.Label className="d-block">
+              <p>Book table for up to 12 guests</p>
+              <Form.Control
+                type="text"
+                name="nameInput"
+                placeholder="Your name"
+                value={form.name}
+                required
+              ></Form.Control>
+              <Form.Control
+                type="number"
+                name="guests"
+                placeholder="Party size"
+                min={1}
+                max={12}
+                value={form.guests}
+                required
+              ></Form.Control>
+              <Form.Control
+                type="datetime-local"
+                name="reservation_time"
+                value={form.reservation_time}
+                required
+              ></Form.Control>
+            </Form.Label>
+          </Form.Group>
+          <Button className="mt-4 float-end">Book</Button>
         </Col>
 
         <Col md={6}>
@@ -30,6 +66,40 @@ export default function BookTablePage() {
             src="/images/BookTableGroup.jpg"
             alt="A group photo of our employees."
           />
+          <Form.Group>
+            <Form.Label className="d-block">
+              <p>Send us an enquiry for bookings of 13+</p>
+              <Form.Control
+                type="text"
+                name="nameInput"
+                placeholder="Your name"
+                value={form.name}
+                required
+              ></Form.Control>
+              <Form.Control
+                type="text"
+                name="emailInput"
+                placeholder="Your email"
+                value={form.email}
+                required
+              ></Form.Control>
+              <Form.Control
+                type="number"
+                name="guests"
+                placeholder="Party size"
+                min={13}
+                value={form.guests}
+                required
+              ></Form.Control>
+              <Form.Control
+                type="datetime-local"
+                name="reservation_time"
+                value={form.reservation_time}
+                required
+              ></Form.Control>
+            </Form.Label>
+          </Form.Group>
+          <Button className="mt-4 float-end">Enquire now</Button>
         </Col>
       </Row>
     </>
