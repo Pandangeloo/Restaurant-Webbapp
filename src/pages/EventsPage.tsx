216e51beb7de6b311d54e522b2b546508b7ce772
Angelica <angelicanorlander@telia.com>
2025-09-23 18:23:31 +0200

    Cleanup : moved form to eventpage

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from "react-bootstrap";
+import { Form, Row, Col, Button, FormSelect } from "react-bootstrap";
 
 EventsPage.route = {
   path: "/events",
@@ -8,12 +8,48 @@ EventsPage.route = {
 
 export default function EventsPage() {
   return (
-    <>
-      <Row>
-        <Col>
-          <h2 className="text-center">Events</h2>
-        </Col>
-      </Row>
-    </>
+    <Row>
+      <Col>
+        <Form>
+          <h1>What kind of event?</h1>
+          <p>After your inquiry we will be in touch in 3-5 workdays</p>
+          <Form.Group>
+            <Form.Label>
+              <FormSelect>
+                <option value="null">Choose your event</option>
+                <option value="null">Wedding</option>
+                <option value="null">Engagement party</option>
+                <option value="null">Private Dinners</option>
+                <option value="null">Party</option>
+              </FormSelect>
+            </Form.Label>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label className="d-block">
+              <p>Your information</p>
+              <Form.Control
+                type="text"
+                name="nameInput"
+                placeholder="First name"
+              ></Form.Control>
+              <Form.Control
+                type="text"
+                name="nameInput"
+                placeholder="Last name"
+              ></Form.Control>
+            </Form.Label>
+            <Form.Label>
+              <Form.Control
+                type="number" // TODO: is this correct??
+                name="mobileNumber"
+                placeholder="Mobilenumber"
+              ></Form.Control>
+            </Form.Label>
+          </Form.Group>
+          <Button className="mt-4 float-end"> Send Inquiry</Button>
+        </Form>
+      </Col>
+    </Row>
   );
 }
