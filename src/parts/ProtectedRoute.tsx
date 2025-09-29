2ac70a4c76eb6e3b5dd8a502ba65c0ceaafccb79
Angelica <angelicanorlander@telia.com>
2025-09-29 16:14:42 +0200

    feat-auth : add protectedRoute for restricting acess depending on userrole. Applied on /admin

---
+++ src/parts/ProtectedRoute.tsx
@@ -0,0 +1,27 @@
+import type { ReactNode } from "react";
+import { useAuth } from "../useAuth";
+import { Navigate } from "react-router-dom";
+
+type ProtectedRouteProps = {
+  children: ReactNode;
+  requiredRole?: "user" | "admin";
+};
+
+export default function ProtectedRoute({
+  children,
+  requiredRole,
+}: ProtectedRouteProps) {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  if (requiredRole && user.role! == requiredRole) {
+    <Navigate to="/" replace />;
+  }
+
+  return <>{children}</>;
+}
